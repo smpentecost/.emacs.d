ad6da2b30fa9ad9fd1abf85d667fc8027e1f7c3c
Seeley <smpentecost@gmail.com>
2020-06-04 14:54:15 -0400

    changed org done mode

--- init.el
+++ init.el
@@ -26,19 +26,22 @@
  '(custom-enabled-themes (quote (wombat)))
  '(elpy-rpc-python-command "python3")
  '(fci-rule-color "#37474f")
+ '(global-auto-revert-mode t)
  '(global-hl-line-mode t)
  '(hl-sexp-background-color "#1c1f26")
+ '(js-indent-level 2)
  '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (python . t))))
  '(org-babel-python-command "python3")
  '(org-edit-src-content-indentation 0)
- '(org-hugo-paired-shortcodes "%tabs/nav")
+ '(org-hugo-paired-shortcodes "%tabs/navmenu %tabs/navmenuitem")
+ '(org-log-done (quote time))
  '(org-modules
    (quote
     (ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus ol-info ol-irc ol-mhe ol-rmail org-tempo ol-w3m)))
  '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
-    (org ac-js2 company xref-js2 js2-refactor toml-mode ox-hugo emacsql-sqlite magit yaml-mode pyenv-mode py-autopep8 flycheck elpy ein better-defaults)))
+    (markdown-mode rjsx-mode org ac-js2 company xref-js2 js2-refactor toml-mode ox-hugo emacsql-sqlite magit yaml-mode pyenv-mode py-autopep8 flycheck elpy ein better-defaults)))
  '(python-shell-interpreter "python3")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -68,3 +71,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(hl-line ((t (:background "gray22")))))
+(put 'narrow-to-region 'disabled nil)
